e3c24d5ef3d0e718756aa344f68f9a51119ccb81
Oscar Monroy <oscarmonroy657@gmail.com>
2022-05-26 11:27:40 -0600

    Basic LazyLoad is working.

--- src/router/Navigation.tsx
+++ src/router/Navigation.tsx
@@ -1,27 +1,30 @@
+import { Suspense } from 'react';
 import { BrowserRouter, Navigate, NavLink, Route, Routes } from 'react-router-dom'
 import logo from '../logo.svg';
 import { routes } from './routes';
 
 export const Navigation = () => {
   return (
-    <BrowserRouter>
-      <div className="main-layout">
-        <nav>
-          <img src={logo} alt="React" />
-          <ul>
-            {routes.map(({ to, name }) => (
-              <li key={to}><NavLink to={to} className={({ isActive }) => isActive ? 'nav-active' : ''}>{name}</NavLink> </li>
-            ))}
-          </ul>
-        </nav>
+    <Suspense>
+      <BrowserRouter>
+        <div className="main-layout">
+          <nav>
+            <img src={logo} alt="React" />
+            <ul>
+              {routes.map(({ to, name }) => (
+                <li key={to}><NavLink to={to} className={({ isActive }) => isActive ? 'nav-active' : ''}>{name}</NavLink> </li>
+              ))}
+            </ul>
+          </nav>
 
-        <Routes>
-          {routes.map(({ path, Component }) => (
-            <Route key={path} path={path} element={<Component/>} />
-          ))}
-          <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+          <Routes>
+            {routes.map(({ path, Component }) => (
+              <Route key={path} path={path} element={<Component />} />
+            ))}
+            <Route path="/*" element={<Navigate to={routes[0].to} replace />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Suspense>
   )
 }
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,29 +1,35 @@
-import { LazyPage1, LazyPage2, LazyPage3 } from '01-lazyLoad/pages';
+import { lazy, LazyExoticComponent } from 'react';
+// import { LazyPage1, LazyPage2, LazyPage3 } from '01-lazyLoad/pages';
 
+type JSXComponent = () => JSX.Element;
 interface Route {
   to: string;
   path: string;
-  Component: () => JSX.Element;
+  Component: JSXComponent | LazyExoticComponent<JSXComponent>;
   name: string;
 }
+
+const Lazy1 = lazy(() => import(/* webpackChunkName: "Lazy1" */'01-lazyLoad/pages/LazyPage1'));
+const Lazy2 = lazy(() => import(/* webpackChunkName: "Lazy2" */'01-lazyLoad/pages/LazyPage2'));
+const Lazy3 = lazy(() => import(/* webpackChunkName: "Lazy3" */'01-lazyLoad/pages/LazyPage3'));
 
 export const routes: Route[] = [
   {
     to: '/lazy1',
     path: 'lazy1',
-    Component: LazyPage1,
+    Component: Lazy1,
     name: 'LazyPage1',
   },
   {
     to: '/lazy2',
     path: 'lazy2',
-    Component: LazyPage2,
+    Component: Lazy2,
     name: 'LazyPage2',
   },
   {
     to: '/lazy3',
     path: 'lazy3',
-    Component: LazyPage3,
+    Component: Lazy3,
     name: 'LazyPage3',
   },
 ]
